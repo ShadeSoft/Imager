44c080e5a96a4c5e34150fa37692ca0b84d61489
Szabolcs Surányi <szabolcs.suranyi@shadesoft.hu>
2019-02-26 16:58:55 +0100

    Working on basics

--- Imager/MainWindow.xaml.cs
+++ Imager/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,43 @@ namespace Imager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly Regex number = new Regex("[^0-9]+");
+
+        private string mode;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private static bool IsTextAllowed(string text)
+        {
+            return !number.IsMatch(text);
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mode_Checked(object sender, RoutedEventArgs e)
+        {
+            mode = ((RadioButton)sender).Name.Replace("mode", "").ToLower();
+            //System.Diagnostics.Debug.WriteLine(mode);
+        }
+
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void BtnFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == true)
+            {
+                textFile.Text = ofd.FileName;
+            }
         }
     }
 }
